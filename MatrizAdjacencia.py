a925b3bc2bb7b3448bdd43e09ecfe333b7e2e99a
Mateus Veloso <mfvelosos@gmail.com>
2018-06-18 17:44:39 -0300

    First Commit

---
+++ MatrizAdjacencia.py
@@ -0,0 +1,73 @@
+from tabulate import tabulate
+
+vertices = []
+
+#Recebendo os Inputs
+while(True):
+    vo = input('Vertice de Origem: ')
+    while(vo == ''):
+        print('Campo dever ser preenchido!')
+        vo = input('Vertice de Origem: ')
+        
+    if(vo == '.'):
+        break
+    
+    vd = input('Vertice de Destino: ')
+    while(vd == ''):
+        print('Campo dever ser preenchido!')
+        vd = input('Vertice de Destino: ')
+        
+    pa = input('Peso da Aresta: ')
+    while(pa == ''):
+        print('Campo dever ser preenchido!')
+        pa = input('Peso da Aresta: ')
+
+    con = [vo.upper(), vd.upper(), pa]
+    vertices.append(con)
+
+    print(len(vertices))
+    print(vertices)
+    for x in range(len(vertices[:-1])):
+        if(con[0] == vertices[x][0]):
+            if(con[1] == vertices[x][1]):
+               print('Invalido! Já existe uma aresta conectando ', con[0],'->',con[1])
+               vertices.pop()
+
+#Coletando cada vértice             
+letras = []
+for l in range(len(vertices)):
+    for m in range(len(vertices[-1])):
+        if(vertices[l][m] not in letras):
+            letras.append(vertices[l][m])
+
+#Removendo números da lista dos vértices
+alfa = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for x in letras:
+    if(x not in alfa):
+        letras.remove(x)
+
+#Ordenando a lista de vértices
+letras.sort()
+print(letras)
+
+#Criando a Matriz
+print(tabulate['a', 'b'],['c', 'd'], headers=letras)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
